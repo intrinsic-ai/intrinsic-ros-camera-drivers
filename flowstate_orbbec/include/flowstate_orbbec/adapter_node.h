1b8ab4d7f052af7b0ed269de2f9d8dc67a253114
Morgan Quigley <morganquigley@intrinsic.ai>
2025-09-11 18:45:25 -0400

    first pass at Describe and Snapshot services

--- flowstate_orbbec/include/flowstate_orbbec/adapter_node.h
+++ flowstate_orbbec/include/flowstate_orbbec/adapter_node.h
@@ -10,6 +10,7 @@
 #include "absl/synchronization/mutex.h"
 #include "orbbec_camera/ob_camera_node_driver.h"
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/camera_info.hpp"
 #include "snapshot_interfaces/srv/describe.hpp"
 #include "snapshot_interfaces/srv/snapshot.hpp"
 
@@ -26,12 +27,44 @@ class AdapterNode : public rclcpp::Node {
   std::unique_ptr<orbbec_camera::OBCameraNodeDriver> orbbec_node_;
   std::thread thread_;
   bool exited_thread_ = false;
-  mutable absl::Mutex mutex_;
 
   rclcpp::Service<snapshot_interfaces::srv::Describe>::SharedPtr
       describe_service_;
   rclcpp::Service<snapshot_interfaces::srv::Snapshot>::SharedPtr
       snapshot_service_;
+
+ private:
+  void DescribeCallback(
+      const std::shared_ptr<rmw_request_id_t> request_header,
+      const std::shared_ptr<snapshot_interfaces::srv::Describe::Request>
+          request,
+      const std::shared_ptr<snapshot_interfaces::srv::Describe::Response>
+          response);
+
+  void SnapshotCallback(
+      const std::shared_ptr<rmw_request_id_t> request_header,
+      const std::shared_ptr<snapshot_interfaces::srv::Snapshot::Request>
+          request,
+      const std::shared_ptr<snapshot_interfaces::srv::Snapshot::Response>
+          response);
+
+  std::string ColorImageTopic() const;
+  std::string IrImageTopic() const;
+
+  mutable absl::Mutex camera_info_mutex_;
+  std::unique_ptr<sensor_msgs::msg::CameraInfo> color_camera_info_;
+  std::unique_ptr<sensor_msgs::msg::CameraInfo> ir_camera_info_;
+  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr color_info_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::CameraInfo>::SharedPtr ir_info_sub_;
+
+  mutable absl::Mutex image_mutex_;
+  std::unique_ptr<sensor_msgs::msg::Image> color_image_;
+  std::unique_ptr<sensor_msgs::msg::Image> ir_image_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr color_image_sub_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr ir_image_sub_;
+
+  mutable absl::Mutex timeout_mutex_;
+  rclcpp::Time t_last_color_image_;
 };
 
 }  // namespace flowstate_orbbec
