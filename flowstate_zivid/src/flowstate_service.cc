6ddc476ce0acd6752138ea48de5d923efe01c4cf
SebastianPrivas <sprivas@google.com>
2026-03-09 15:42:09 +0000

    fix StatusOr object usage in zivid main

--- flowstate_zivid/src/flowstate_service.cc
+++ flowstate_zivid/src/flowstate_service.cc
@@ -5,13 +5,18 @@
 
 int main(int argc, char** argv) {
   rclcpp::init(argc, argv);
-  auto node = flowstate_zivid::SpawnerNode::Create();
-  if (!node.ok()) {
-    RCLCPP_ERROR_STREAM(rclcpp::get_logger("main"),
-                        "Failed to create SpawnerNode: " << node.status());
+
+  auto node_status = flowstate_zivid::SpawnerNode::Create();
+  if (!node_status.ok()) {
+    RCLCPP_ERROR_STREAM(
+        rclcpp::get_logger("main"),
+        "Failed to create SpawnerNode: " << node_status.status());
     return EXIT_FAILURE;
   }
-  node->UpdateCameras();
-  rclcpp::spin(*node);
+
+  std::shared_ptr<flowstate_zivid::SpawnerNode> spawner = node_status.value();
+
+  spawner->UpdateCameras();
+  rclcpp::spin(spawner);
   rclcpp::shutdown();
 }
