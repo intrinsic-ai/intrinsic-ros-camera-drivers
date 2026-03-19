3e7e05630f665e273f8348dc36cd13276203ed32
SebastianPrivas <sprivas@google.com>
2026-03-19 09:58:22 +0000

    fix missing packages for zivid docker

--- flowstate_zivid/flowstate/build_service_bundle.sh
+++ flowstate_zivid/flowstate/build_service_bundle.sh
@@ -8,5 +8,5 @@ set -o errexit
 set -o verbose
 
 src/sdk-ros/scripts/setup_docker.sh
-src/sdk-ros/scripts/build_container.sh --service_name zivid_driver --service_package flowstate_zivid --dockerfile src/flowstate-ros-camera-drivers/flowstate_zivid/flowstate/Dockerfile.service
+src/sdk-ros/scripts/build_container.sh --service_name zivid_driver --service_package flowstate_zivid --dockerfile src/flowstate-ros-camera-drivers/flowstate_zivid/flowstate/Dockerfile.flowstate_service
 src/sdk-ros/scripts/build_bundle.sh --service_name zivid_driver --service_package flowstate_zivid
