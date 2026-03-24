2d6719251acbdb98dce7aff91f4c629d941fbbfc
SebastianPrivas <sprivas@google.com>
2026-03-24 09:25:30 +0000

    enforce nodejs 24

--- flowstate_common/include/flowstate_common/base_adapter_node.h
+++ flowstate_common/include/flowstate_common/base_adapter_node.h
@@ -19,7 +19,7 @@
 #include "snapshot_interfaces/srv/snapshot.hpp"
 
 namespace flowstate_common {
-// Add small comment
+// Add
 /**
  * @class BaseAdapterNode
  * @brief Abstract base class for camera adapter nodes that implement the
